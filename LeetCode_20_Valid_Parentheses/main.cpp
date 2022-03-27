1a0d1a90cd052819568db63d048b64fb7e044198
woosoojin <wsj1998@naver.com>
2022-03-27 23:08:20 +0900

    Valid Parentheses

---
+++ LeetCode_20_Valid_Parentheses/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> parentheses;
+
+        char bracket;
+        for (int i = 0; i < s.length(); i++) {
+            bracket = s[i];
+            
+            // ( or [ or {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                parentheses.push(bracket);
+            }
+            // ) or ] or }
+            else {
+                if (!parentheses.empty()) {
+                    char temp;
+                    temp = parentheses.top();
+
+                    if (temp == '(' && bracket == ')') {
+                        parentheses.pop();
+                    }
+                    else if (temp == '{' && bracket == '}')
+                    {
+                        parentheses.pop();
+                    }
+                    else if (temp == '[' && bracket == ']')
+                    {
+                        parentheses.pop();
+                    }
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+        }
+        return parentheses.empty();
+    }
+};
+
+int main() {
+    string s;
+    cin >> s;
+
+    Solution sol;
+    cout << sol.isValid(s) << endl;
+
+    return 0;
+}
